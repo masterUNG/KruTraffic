1049874a3f57b2cdafad3bac848353bcbcf01e2b
masterUNG <phrombutr@gmail.com>
2015-09-14 15:10:20 +0700

    Web View

--- app/src/main/java/appewtc/masterung/krutraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/krutraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package appewtc.masterung.krutraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -21,7 +25,31 @@ public class MainActivity extends AppCompatActivity {
         //Initial Widget
         initialWidget();
 
+        //Button Controller
+        buttonController();
+
+
     }   // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                buttonMediaPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/K2BT1ZcYsMc"));
+                startActivity(objIntent);
+
+            }   // event
+        });
+
+    }   //buttonController
 
     private void initialWidget() {
 
